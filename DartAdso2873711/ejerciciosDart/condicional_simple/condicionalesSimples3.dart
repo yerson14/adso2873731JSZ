9d905b9ad4b2c5a0df58ad8df0127b4b7b417e25
yerson14 <yerson_herrera@soy.sena.edu.co>
2024-05-04 22:34:45 -0500

    Finiquitado

--- DartAdso2873711/ejerciciosDart/condicional_simple/condicionalesSimples3.dart
+++ DartAdso2873711/ejerciciosDart/condicional_simple/condicionalesSimples3.dart
@@ -1,12 +1,25 @@
+import 'dart:io';
+
 void main() {
   //Yerson Herrera - EJE condicional simple  03
   /*
   El jefe deunalmacén de ropa, pone una promoción en sus trajes por un período de tres días para sus clientes, de tal manera que si un cliente ordena un traje se captura el modelo del traje y el precio unitario. Si el cliente ordena tres o más trajes se le hace un descuento del 17%, si no se le cobra al precio normal
   */
   //Definicion variables
+  int cantidadTrajes;
+  double precioTraje, totalPagar;
 
-  //Entrada/Proceso alg
+  //Entrada
+print("Cuantos trajes desea comprar: ");
+cantidadTrajes = int.parse(stdin.readLineSync()!);
 
+//Proceso alg
+precioTraje = 120000;
+totalPagar = precioTraje * cantidadTrajes;
+if (cantidadTrajes >= 3) {
+  totalPagar = totalPagar - (totalPagar * 0.17);
+}
 //Salida alg
+print("El total a pagar es de: $totalPagar");
   
 }
