6924bf61c29dab4c65826896349bc4c5dfe6ac40
unknown <SENA@MANAPRCPIFSD001.FormacionSena.RED>
2024-05-07 08:14:19 -0500

    Ciclos ejemplos

---
+++ DartAdso2873711/Ciclos/ejemplosCiclos/ejemplosFOR_01.dart
@@ -0,0 +1,6 @@
+void main(List<String> args) {
+  print("Imprimir los numeros del 1 al 5");
+  for(int i=1; i<=5; i++){
+    print("i: $i");
+  }
+}
---
+++ DartAdso2873711/Ciclos/ejemplosCiclos/ejemplosFOR_02.dart
@@ -0,0 +1,6 @@
+void main(List<String> args) {
+  print("Imprime numeros el 0 al 25, de dos en dos");
+  for(int i=0; i<=25; i=i+2){
+    print("i: $i");
+  }
+}
