c501d0d89d506ef63fedfe9eae27e3ce734cee09
unknown <SENA@MANAPRCPIFSD001.FormacionSena.RED>
2024-05-09 11:52:17 -0500

    Se inicio la guia

--- DartAdso2873711/Ciclos/ejemplosCiclos/ejemplosWHILE_03.dart
+++ DartAdso2873711/Ciclos/ejemplosCiclos/ejemplosWHILE_03.dart
@@ -2,15 +2,5 @@ import 'dart:io';
 
 void main(List<String> args) {
 
-print("imprimir numeros pares digitados por el usuario");
-int num=-1;
-while (num !=0) {
-print("Ingrese un numero (Cero para salir)");
-num = int.parse(stdin.readLineSync()!);
- if(num % 2 ==0){//Comprobar si es par
-  print("El numero es par");
- }else{
-  print("El mumero es impar");
- }
-}
+
 }
---
+++ DartAdso2873711/Ciclos/ejrciciosGuia04Dart/WHILE/ejercicioWHILE01.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+void main(List<String> args) {
+
+    /*
+    Una compañía de seguros tiene contratados a n vendedores. Cada uno hace tres ventas a la semana. Su política de pagos es que un vendedor recibe un sueldo base, y un 10% extra por comisiones de sus  ventas.  El  gerente  de  su  compañía  desea  saber  cuanto  dineroobtendrá  en  la  semana  cada vendedor por concepto de comisiones por las tres ventas realizadas, y cuanto tomando en cuenta su sueldo base y sus comisiones
+    */
+
+    //Definicion de variables
+
+    int numVendedores,contador=0,sueldo,cantidad=3 ;
+    double comisiones,ventas,sueldoTotal;
+
+    print("Digite la cantidad de trbajadores contratados");
+    numVendedores=int.parse(stdin.readLineSync()!);
+    while(contador <numVendedores){
+      print("Digite el sueldo base");
+      sueldo=int.parse(stdin.readLineSync()!);
+      for(int i=0; i<cantidad;i++){
+        print("Ingrese el valor de la venta numero "+(i+1).toString());
+        ventas=double.parse(stdin.readLineSync()!);
+        sueldo+=cantidad;
+        contador++;
+      }
+      comisiones=sueldo*0.10;
+      print("El total con comisiones es: $comisiones ");
+      sueldoTotal=sueldo+comisiones;
+      print("El sueldo total es de $sueldoTotal");
+    }
+}
