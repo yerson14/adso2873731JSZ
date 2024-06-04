1aa6f569c51886af5f732d61d18dafcdb33bd4d7
Jumaga11 <Tecnolojuancho@live.com>
2024-06-04 11:45:58 -0500

    actualizacion de arreglos

--- DartAdso2873711/listasArreglos/ejerciciosGuia05/ejerciciosVector05.dart
+++ DartAdso2873711/listasArreglos/ejerciciosGuia05/ejerciciosVector05.dart
@@ -4,14 +4,28 @@ void main(List<String> args) {
   /*
   Diseñe  un  algoritmo  que  lea  un  número  cualquiera  y  lo  busque  en  el  vector  X,  el  cual  tiene almacenados  12  elementos.  Escribir  la  posición  donde  se  encuentra  almacenado  el  número  en  el vector o el mensaje “NO” si no lo encuentra
   */
-  List<double> vector=[];
+  List<int> vector=[];
   int cantNumeros=12;
-  double num,posicion=0;
+  int num,posicion=-1,numBuscar;
 
   for (var i = 0; i < cantNumeros; i++) {
     print("Digite el numero ${i+1}");
-    num=double.parse(stdin.readLineSync()!);
+    num=int.parse(stdin.readLineSync()!);
     vector.add(num);
-    
+  }
+  print("ingrese el umero que desea buscar em el vector");
+  numBuscar=int.parse(stdin.readLineSync()!);
+
+  //Ciclo para busca el elemento
+  for (var i = 0; i < vector.length; i++) {
+    if (numBuscar == vector[i]) {
+      posicion=i;
+      //Break esto ropmpe cualquier ciclo
+    }
+    if (posicion !=-1) {
+      print("El numero se encuentra en la poscion $posicion");
+    }else{
+      print("El numero no se encontro");
+    }
   }
 }
---
+++ DartAdso2873711/listasArreglos/ejerciciosGuia05/ejerciciosVector06.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  /*
+  Diseñe  un  algoritmo  que  lea  un  número  cualquiera  y  lo  busque  en  el  vector  X,  el  cual  tiene almacenados  12  elementos.  Escribir  la  posición  donde  se  encuentra  almacenado  el  número  en  el vector o el mensaje “NO” si no lo encuentra
+  */
+  
+
+}
